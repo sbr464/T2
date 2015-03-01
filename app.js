5cb1d7aa2468d2f852712cbc6183a5635d443aa5
Stephen Richardson <sbr464@gmail.com>
2015-03-01 10:09:11 -0700

    updates

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var indexController = require('./controllers/index.js');
+
+var app = express();
+app.set('view engine', 'jade');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/bower_components'));
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.get('/', indexController.index);
+
+var server = app.listen(7621, function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
